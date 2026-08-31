a5b68315db0c31c1a77dfad2ef510e0a2a7bef61
Joost Oudejans <3450747-S2108887@users.noreply.gitlab.com>
2026-08-31 21:49:14 +0200

    - Navigatieverbetering: Knop krijgt status 'actief' - De onjuiste verwijzingen naar lettertypebestanden zijn gecorrigeerd.

--- scripts/script.js
+++ scripts/script.js
@@ -12,17 +12,26 @@ let dlLijst = document.querySelectorAll('dl')
 let navigatieknoppen = document.querySelectorAll("nav > button")
 
 dlLijst.forEach(el => verbergElement(el));
+toonDefinitieLijst(0);
 
 navigatieknoppen.forEach((knop, nummer) => {
   knop.addEventListener("click", () => {
-    toonDlLijst(nummer)
+    toonDefinitieLijst(nummer)
   })
   
 })
 
-function toonDlLijst(nummer) {
-  console.log('toonElement ' + nummer)
-  console.log('lengte: ' + dlLijst.length);
+function toonDefinitieLijst(nummer) {
+  navigatieknoppen.forEach((knop, i) => {
+    if (i === nummer) {
+      knop.classList.add('actief')
+    }
+    else {
+      knop.classList.remove('actief')
+    }
+  })
+  
+  
   dlLijst.forEach((dl, i) => {
     if (i === nummer) {
       toonElement(dl);
