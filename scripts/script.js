33a155ca5e058421ee7e88fb9b487f2954ec03f3
Joost Oudejans <3450747-S2108887@users.noreply.gitlab.com>
2026-08-31 16:03:04 +0200

    Er zijn navigatieknoppen toegevoegd. De inhoud is echter nog verkeerd.

--- scripts/script.js
+++ scripts/script.js
@@ -7,3 +7,39 @@ expandButton.addEventListener('click', function() {
   /* Stap 3: Verander een class zodra iemand op die knop klikt */
   document.body.classList.toggle('is-expanded')
 })
+
+let dlLijst = document.querySelectorAll('dl')
+let navigatieknoppen = document.querySelectorAll("nav > button")
+
+dlLijst.forEach(el => verbergElement(el));
+
+navigatieknoppen.forEach((knop, nummer) => {
+  knop.addEventListener("click", () => {
+    toonDlLijst(nummer)
+  })
+  
+})
+
+function toonDlLijst(nummer) {
+  console.log('toonElement ' + nummer)
+  console.log('lengte: ' + dlLijst.length);
+  dlLijst.forEach((dl, i) => {
+    if (i === nummer) {
+      toonElement(dl);
+    }
+    else {
+      verbergElement(dl);
+    }
+  })
+}
+
+function toonElement(element) {
+  element.removeAttribute("style");
+}
+
+function verbergElement(element) {
+  element.setAttribute("style", "display: none;")
+}
+
+
+
