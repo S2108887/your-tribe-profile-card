37922d1e40f6ecb6ae0c6101f4d3b7c7deb3f147
Joost Oudejans <3450747-S2108887@users.noreply.gitlab.com>
2026-09-01 15:21:10 +0200

    Javascript wordt niet meer gebruikt voor de navigatie. De inhoud en opmaak is nog steeds niet goed.

--- scripts/script.js
+++ scripts/script.js
@@ -8,47 +8,5 @@ expandButton.addEventListener('click', function() {
   document.body.classList.toggle('is-expanded')
 })
 
-let dlLijst = document.querySelectorAll('dl')
-let navigatieknoppen = document.querySelectorAll("nav > button")
-
-dlLijst.forEach(el => verbergElement(el));
-toonDefinitieLijst(0);
-
-navigatieknoppen.forEach((knop, nummer) => {
-  knop.addEventListener("click", () => {
-    toonDefinitieLijst(nummer)
-  })
-  
-})
-
-function toonDefinitieLijst(nummer) {
-  navigatieknoppen.forEach((knop, i) => {
-    if (i === nummer) {
-      knop.classList.add('actief')
-    }
-    else {
-      knop.classList.remove('actief')
-    }
-  })
-  
-  
-  dlLijst.forEach((dl, i) => {
-    if (i === nummer) {
-      toonElement(dl);
-    }
-    else {
-      verbergElement(dl);
-    }
-  })
-}
-
-function toonElement(element) {
-  element.removeAttribute("style");
-}
-
-function verbergElement(element) {
-  element.setAttribute("style", "display: none;")
-}
-
 
 
